c8fbefbbf5103fd95a57902e5f2c1e63b8a9ab76
AbhinandG <81344251+AbhinandG@users.noreply.github.com>
2022-04-08 11:11:34 +0530

    extra features

--- app.py
+++ app.py
@@ -9,12 +9,23 @@ from textblob import TextBlob
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-
+import pickle
+import math
+import joblib
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+from nltk.tokenize import sent_tokenize, word_tokenize
+import nltk
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.base import TransformerMixin 
 
 
 st.markdown('# **Socially Relevant Project - Automatic Video Transcript Generator**')
+st.markdown('**Abhinand G (2019115003) - 6th Semester, BTech IT**')
 
-st.markdown('**Abhinand G (2019115003)**')
 bar = st.progress(0)
 
 
@@ -27,23 +38,90 @@ def get_yt(URL):
 
 
 def sentiment_generator(transcri):
+    saved_model=joblib.load('ML_model.pkl')
+    trainin_df=pd.read_csv('training.1600000.processed.noemoticon.csv')
+    print("==DATASET IMPORTED==")
+    trains_df=pd.DataFrame()
+    trains_df['content']=trainin_df.iloc[:,5]
+    trains_df['target']=trainin_df.iloc[:,0]
+    stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an',
+             'and','any','are', 'as', 'at', 'be', 'because', 'been', 'before',
+             'being', 'below', 'between','both', 'by', 'can', 'd', 'did', 'do',
+             'does', 'doing', 'down', 'during', 'each','few', 'for', 'from',
+             'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
+             'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
+             'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
+             'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once',
+             'only', 'or', 'other', 'our', 'ours','ourselves', 'out', 'own', 're','s', 'same', 'she', "shes", 'should', "shouldve",'so', 'some', 'such',
+             't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
+             'themselves', 'then', 'there', 'these', 'they', 'this', 'those',
+             'through', 'to', 'too','under', 'until', 'up', 've', 'very', 'was',
+             'we', 'were', 'what', 'when', 'where','which','while', 'who', 'whom',
+             'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
+             "youve", 'your', 'yours','must', 'yourself', 'yourselves']
+
+    STOPWORDS = set(stopwordlist)
+    def cleaning_stopwords(text):
+        return " ".join([word for word in str(text).split() if word not in STOPWORDS])
+        trains_df['content'] = trains_df['content'].apply(lambda text: cleaning_stopwords(text))
+
+    def cleaning_URLs(data):
+        return re.sub('((www.[^s]+)|(https?://[^s]+))',' ',data)
+        trains_df['content'] = trains_df['content'].apply(lambda x: cleaning_URLs(x))
+
+    def cleaning_ATS(data):
+        return re.sub('RT @[\w]+',' ',data)
+        trains_df['content'] = trains_df['content'].apply(lambda x: cleaning_ATS(x))
+
+    def cleaning_SPL(data):
+        return re.sub('[^A-Za-z]',' ',data)
+        trains_df['content'] = trains_df['content'].apply(lambda x: cleaning_SPL(x))
+
+    print("==DATA CLEANED==")
+ 
+    X=trains_df['content']
+    y=trains_df['target']
+
+    print("==SPLIT INTO X AND Y==")
+
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.05, random_state =26105111)
+
+    vectoriser = TfidfVectorizer(ngram_range=(1,2), max_features=500000)
+    vectoriser.fit(X_train)
+
+    print("TFIDF HAS BEEN FIT")
+
+    new_dataframe={'content':transcri, 'target':5}
+    print("DATAFRAME CREATED")
+    new_dataframe=pd.DataFrame(new_dataframe,index=[0])
+    X_test1=new_dataframe['content']
+    X_test1=vectoriser.transform(X_test1)
+    predicted_score=saved_model.predict(X_test1)
+
     polarity=TextBlob(transcri).sentiment.polarity
-    subjectivity=TextBlob(transcri).sentiment.polarity
-    if polarity>0:
+    subjectivity=TextBlob(transcri).sentiment.subjectivity
+    if predicted_score[0]==4:
         st.success('The video has a positive sentiment with polarity value '+str(polarity))
+        barpolarity=st.progress(0)
+        barpolarity.progress(math.floor(polarity*100))
     else:
         st.error('The video has a negative sentiment with polarity value '+str(polarity))
-
-    chart_data=pd.DataFrame()
-    chart_data['Polarity']=polarity
-    chart_data['Subjectivity']=subjectivity
-    
-    st.line_chart(chart_data)
+        barpolarity=st.progress(0)
+        barpolarity.progress(math.floor(polarity*100))
+    if subjectivity>0.5:
+        st.success('The subjectivity score is '+str(subjectivity))
+        barsubjectivity=st.progress(0)
+        barsubjectivity.progress(math.floor(subjectivity*100))
+    else:
+        st.error('The subjectivity score is '+str(subjectivity))
+        barsubjectivity=st.progress(0)
+        barsubjectivity.progress(math.floor(subjectivity*100))
+    details={"Polarity":polarity, "Subjectivity":subjectivity}
+    chart_data=pd.DataFrame(details,index=[0]) 
+    st.bar_chart(chart_data)
 
 def transcribe_yt():
-
     current_dir = os.getcwd()
-
     for file in os.listdir(current_dir):
        #if file.endswith(".mp4"):
        if file.startswith(fileToUpload.name):
@@ -51,9 +129,6 @@ def transcribe_yt():
             #print(mp4_file)
     filename = mp4_file
     bar.progress(20)
-
-
-
     def read_file(filename, chunk_size=5242880):
         with open(filename, 'rb') as _file:
             while True:
@@ -83,9 +158,7 @@ def transcribe_yt():
 
     transcript_input_response = requests.post(endpoint, json=json, headers=headers)
 
-
     bar.progress(40)
-
 
     transcript_id = transcript_input_response.json()["id"]
 
@@ -99,7 +172,6 @@ def transcribe_yt():
 
     bar.progress(60)
 
-
     from time import sleep
 
     st.warning('Transcription is processing ...')
@@ -110,20 +182,14 @@ def transcribe_yt():
     
     bar.progress(100)
 
-
     st.header('Output')
-
 
     st.info(transcript_output_response.json()["text"])
     sentiment_generator(transcript_output_response.json()["text"])
 
-   
-
-  
     yt_txt = open('yt.txt', 'w')
     yt_txt.write(transcript_output_response.json()["text"])
     yt_txt.close()
-
 
     srt_endpoint = endpoint + "/srt"
     srt_response = requests.get(srt_endpoint, headers=headers)
@@ -135,13 +201,8 @@ def transcribe_yt():
     zip_file.write('yt.srt')
     zip_file.close()
 
-
-
-
-
-
-st.sidebar.header('Which video do you want to analyze?')
-
+with st.sidebar:
+    st.write('**Which video do you want to analyse?**')
 
 with st.sidebar.form(key='my_form'):
 	#URL = st.text_input('Enter URL of YouTube video:')
@@ -149,7 +210,11 @@ with st.sidebar.form(key='my_form'):
     fileToUpload = st.file_uploader("Upload a custom video..", type=["mp4"])
     submit_button=st.form_submit_button(label='Go')
 
+with st.sidebar:
+    st.write('This is web application that inputs .mp4 files to generate transcripts via the AssemblyAI API, and makes use of pre-trained models to predict sentiments of the transcripts and analyses the amount of profanity used in the video.')
 
+with st.sidebar:
+    st.write('Developed by: abhinandganesh2001@gmail.com')
 
 if submit_button:
     #get_yt(URL)
@@ -168,3 +233,12 @@ if submit_button:
             file_name="transcription.zip",
             mime="application/zip"
         )
+
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
---
+++ mlmodel.py
@@ -0,0 +1,111 @@
+from textblob import TextBlob
+import pandas as pd
+from sklearn.metrics import confusion_matrix, classification_report
+import numpy as np
+from nltk.tokenize import sent_tokenize, word_tokenize
+import nltk
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.pipeline import Pipeline 
+from sklearn.base import TransformerMixin 
+import pickle
+import joblib
+
+
+print("==PACKAGES IMPORTED==")
+
+trainin_df=pd.read_csv('training.1600000.processed.noemoticon.csv')
+
+print("==DATASET IMPORTED==")
+
+trains_df=pd.DataFrame()
+trains_df['content']=trainin_df.iloc[:,5]
+
+trains_df['target']=trainin_df.iloc[:,0]
+
+stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an',
+             'and','any','are', 'as', 'at', 'be', 'because', 'been', 'before',
+             'being', 'below', 'between','both', 'by', 'can', 'd', 'did', 'do',
+             'does', 'doing', 'down', 'during', 'each','few', 'for', 'from',
+             'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
+             'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
+             'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
+             'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once',
+             'only', 'or', 'other', 'our', 'ours','ourselves', 'out', 'own', 're','s', 'same', 'she', "shes", 'should', "shouldve",'so', 'some', 'such',
+             't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
+             'themselves', 'then', 'there', 'these', 'they', 'this', 'those',
+             'through', 'to', 'too','under', 'until', 'up', 've', 'very', 'was',
+             'we', 'were', 'what', 'when', 'where','which','while', 'who', 'whom',
+             'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
+             "youve", 'your', 'yours','must', 'yourself', 'yourselves']
+
+STOPWORDS = set(stopwordlist)
+def cleaning_stopwords(text):
+    return " ".join([word for word in str(text).split() if word not in STOPWORDS])
+trains_df['content'] = trains_df['content'].apply(lambda text: cleaning_stopwords(text))
+
+
+def cleaning_URLs(data):
+    return re.sub('((www.[^s]+)|(https?://[^s]+))',' ',data)
+trains_df['content'] = trains_df['content'].apply(lambda x: cleaning_URLs(x))
+
+
+def cleaning_ATS(data):
+    return re.sub('RT @[\w]+',' ',data)
+trains_df['content'] = trains_df['content'].apply(lambda x: cleaning_ATS(x))
+
+
+def cleaning_SPL(data):
+    return re.sub('[^A-Za-z]',' ',data)
+trains_df['content'] = trains_df['content'].apply(lambda x: cleaning_SPL(x))
+
+
+print("==DATA CLEANED==")
+
+X=trains_df['content']
+y=trains_df['target']
+
+print("==SPLIT INTO X AND Y==")
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.05, random_state =26105111)
+
+vectoriser = TfidfVectorizer(ngram_range=(1,2), max_features=500000)
+vectoriser.fit(X_train)
+print('No. of feature_words: ', len(vectoriser.get_feature_names()))
+
+
+X_train = vectoriser.transform(X_train)
+X_test  = vectoriser.transform(X_test)
+
+def model_Evaluate(model):
+    y_pred = model.predict(X_test)
+    print(classification_report(y_test, y_pred))
+    cf_matrix = confusion_matrix(y_test, y_pred)
+    categories = ['Negative','Positive']
+    group_names = ['True Neg','False Pos', 'False Neg','True Pos']
+    group_percentages = ['{0:.2%}'.format(value) for value in cf_matrix.flatten() / np.sum(cf_matrix)]
+    labels = [f'{v1}n{v2}' for v1, v2 in zip(group_names,group_percentages)]
+    labels = np.asarray(labels).reshape(2,2)
+    sns.heatmap(cf_matrix, annot = labels, cmap = 'Blues',fmt = '',
+    xticklabels = categories, yticklabels = categories)
+    plt.xlabel("Predicted values", fontdict = {'size':14}, labelpad = 10)
+    plt.ylabel("Actual values" , fontdict = {'size':14}, labelpad = 10)
+    plt.title ("Confusion Matrix", fontdict = {'size':18}, pad = 20)
+
+LR_model = LogisticRegression(solver='lbfgs')
+LR_model.fit(X_train, y_train)
+model_Evaluate(LR_model)
+y_predict_lr = LR_model.predict(X_test)
+print("ACCURACY SCORE IS: "+str(accuracy_score(y_test, y_predict_lr)))
+
+joblib.dump(LR_model,'ML_model.pkl')
+
+print("==MODEL PICKLED==")
